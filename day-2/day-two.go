ae76fd5aa5b698f8140faf482339c401fe099572
Omar Baig <OmarBaig1998@gmail.com>
2024-12-02 19:08:50 -0600

    trying recursion

--- day-2/day-two.go
+++ day-2/day-two.go
@@ -9,6 +9,44 @@ import (
 	"strings"
 )
 
+func checkReactorReport(report []string, numProblematic int) int {
+
+	fmt.Println(report, numProblematic)
+
+	if len(report) == 1 {
+		return numProblematic
+	}
+
+	var prev int
+	var shouldIncrease bool
+	for i := range len(report) {
+		curr := helpers.StringToInt(report[i])
+		if i > 0 {
+			// on the first element, determine whether the reports or increasing or decreasing
+			if i == 1 {
+				shouldIncrease = curr > prev
+			} else {
+				// check to see if the code is increasing or not when it should/should not
+				if (shouldIncrease && prev >= curr) || (!shouldIncrease && curr >= prev) {
+					newReport := append(report[:i], report[i+1:]...)
+					return checkReactorReport(newReport, numProblematic+1)
+				}
+			}
+
+			diff := int(math.Abs(float64(curr - prev)))
+			// if the diff is not at least one and more than 3, then break out of the loop
+			if diff < 1 || diff > 3 {
+				newReport := append(report[:i], report[i+1:]...)
+				return checkReactorReport(newReport, numProblematic+1)
+			}
+		}
+
+		prev = curr
+	}
+
+	return numProblematic
+}
+
 func main() {
 
 	inFile := os.Args[1]
@@ -31,34 +69,10 @@ func main() {
 
 	numSafeReports := 0
 	for i := range len(reports) {
-		var prev int
-		var shouldIncrease bool
-		for j := range len(reports[i]) {
-			curr := helpers.StringToInt(reports[i][j])
-			if j > 0 {
-				// on the first element, determine whether the reports or increasing or decreasing
-				if j == 1 {
-					shouldIncrease = curr > prev
-				} else {
-					// check to see if the code is increasing or not when it should/should not
-					if (shouldIncrease && prev >= curr) || (!shouldIncrease && curr >= prev) {
-						break
-					}
-				}
-
-				diff := int(math.Abs(float64(curr - prev)))
-
-				// if the diff is not at least one and more than 3, then break out of the loop
-				if diff < 1 || diff > 3 {
-					break
-				}
-			}
-
-			// if the code made it here, then it's a safe report
-			if j == len(reports[i])-1 {
-				numSafeReports++
-			}
-			prev = curr
+		result := checkReactorReport(reports[i], 0)
+		fmt.Println(" ")
+		if result <= 1 {
+			numSafeReports++
 		}
 	}
 
